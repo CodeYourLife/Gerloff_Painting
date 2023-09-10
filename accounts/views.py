61a6a0082609febd9abf6869e2fbad964bcefdd1
Anthony Taroli <taroli757@gmail.com>
2023-09-09 22:32:46 -0400

    GP-6: Introduce add_employee, registration, and verify_pin pages to application (#3)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+def registration(request):
+    send_data = {}
+    #send_data['employees']=Employees.objects.filter(active = True)
+    return render(request, "registration.html", send_data)
+
+def verifyPin(request):
+    send_data = {}
+    #send_data['employees']=Employees.objects.filter(active = True)
+    return render(request, "verify_pin.html", send_data)
+
+def addEmployee(request):
+    send_data = {}
+    #send_data['employees']=Employees.objects.filter(active = True)
+    return render(request, "add_employee.html", send_data)
+
+def addNewEmployee(request):
+    print('hello')
+    if request.method == 'POST':
+        firstName = request.POST['firstName']
+        lastName = request.POST['lastName']
+        print('here', firstName, lastName)
+
