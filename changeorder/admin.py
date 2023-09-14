f5752f21bf1daf9b4fdb8f4d80d782f2af29fbb6
Anthony Taroli <taroli757@gmail.com>
2023-09-14 11:47:07 -0400

    Introduce updated booknewjob styling, Introduce registred models to respective apps

--- changeorder/admin.py
+++ changeorder/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(TMPricesMaster)
+admin.site.register(TMList)
+admin.site.register(EWTicket)
+admin.site.register(EWT)
+admin.site.register(ClientJobRoles)
+admin.site.register(ChangeOrders)
+admin.site.register(TempRecipients)
+
+
+
--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Jobs)
+admin.site.register(JobNumbers)
+admin.site.register(JobNotes)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
+admin.site.register(Clients)
+admin.site.register(ClientEmployees)
+admin.site.register(Estimates)
+admin.site.register(Plans)
+admin.site.register(JobCharges)
+
