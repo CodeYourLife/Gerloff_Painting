ff654ac136a4fdf7c0606b6cf4212de3c0d28af6
Anthony Taroli <taroli757@gmail.com>
2023-09-18 12:26:35 -0400

    GP-24: Introduce sub navigation sizing based on screen size

--- trinity/settings.py
+++ trinity/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3t5e#t@#xt)=#557w!n_y@vrm-c8x7_71z=8tw-0+(_^&6&$_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.168.120', '98.174.216.233']
 
@@ -139,6 +139,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STATICFILES_DIR = (
+    '/jobs/static/jobs/project_styles.css'
+)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # For Emails
