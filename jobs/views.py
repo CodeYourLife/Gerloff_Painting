4f669ba2849ac97261c2316ca9332f75a2d8547d
Anthony Taroli <taroli757@gmail.com>
2023-09-23 22:42:32 -0400

    job booked first time. changed jobs model to allow null PM (#14)
    Co-authored-by: joe <joe@gerloffpainting.com>

--- jobs/views.py
+++ jobs/views.py
@@ -17,39 +17,46 @@ from submittals.models import *
 from console.misc import Email
 
 
-def change_start_date(request,jobnumber,previous):
+def change_start_date(request, jobnumber, previous):
     jobs = Jobs.objects.get(job_number=jobnumber)
     format_date = jobs.start_date.strftime("%Y-%m-%d")
     previous_page = previous
     if request.method == 'POST':
-        jobs.start_date=request.POST['start_date']
+        jobs.start_date = request.POST['start_date']
         if 'is_active' in request.POST:
-            jobs.is_active=True
+            jobs.is_active = True
         jobs.start_date_checked = date.today()
-        new_note = JobNotes.objects.create(job_number=jobs, note=request.POST['date_note'],type="auto_start_date_note",user =request.user.first_name+request.user.last_name, date=date.today())
+        new_note = JobNotes.objects.create(job_number=jobs, note=request.POST['date_note'], type="auto_start_date_note",
+                                           user=request.user.first_name + request.user.last_name, date=date.today())
         new_note.save()
         jobs.save()
         if previous == 'jobpage':
-            return redirect('job_page',jobnumber='ALL')
+            return redirect('job_page', jobnumber='ALL')
         else:
             return redirect('super_home')
-    return render(request, "change_start_date.html", {'jobs': jobs,'formatdate': format_date, 'previous_page': previous_page})
-def update_job_info(request,jobnumber):
+    return render(request, "change_start_date.html",
+                  {'jobs': jobs, 'formatdate': format_date, 'previous_page': previous_page})
+
+
+def update_job_info(request, jobnumber):
     return render(request, "update_job_info.html")
+
 
 def jobs_home(request):
     response = redirect('/')
     return response
 
 
-def job_page(request,jobnumber):
+def job_page(request, jobnumber):
     if jobnumber == 'ALL':
         search_jobs = JobsFilter(request.GET, queryset=Jobs.objects.filter(status="Open"))
         jobstable = JobsTable(search_jobs.qs)
         has_filter = any(field in request.GET for field in set(search_jobs.get_fields()))
         tickets = ChangeOrders.objects.filter(job_number__status="Open", is_t_and_m=True, is_ticket_signed=False)
-        open_cos = ChangeOrders.objects.filter(job_number__status="Open", is_closed=False, is_approved=False) & ChangeOrders.objects.filter(is_t_and_m=False) | ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=True)
-        approved_cos =  ChangeOrders.objects.filter(job_number__status="Open",is_closed=False,is_approved=True)
+        open_cos = ChangeOrders.objects.filter(job_number__status="Open", is_closed=False,
+                                               is_approved=False) & ChangeOrders.objects.filter(
+            is_t_and_m=False) | ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=True)
+        approved_cos = ChangeOrders.objects.filter(job_number__status="Open", is_closed=False, is_approved=True)
         equipment = Inventory.objects.filter(job_number__status="Open").order_by('inventory_type')
         rentals = Rentals.objects.filter(job_number__status="Open")
         wallcovering2 = Wallcovering.objects.filter(job_number__status="Open")
@@ -75,11 +82,13 @@ def job_page(request,jobnumber):
         jobstable = JobsTable(Jobs.objects.filter(job_number=jobnumber))
         jobs = Jobs.objects.filter(job_number=jobnumber)[0:2000]
         tickets = ChangeOrders.objects.filter(job_number=jobnumber, is_t_and_m=True, is_ticket_signed=False)
-        open_cos = ChangeOrders.objects.filter(job_number=jobnumber, is_closed=False, is_approved=False) & ChangeOrders.objects.filter(is_t_and_m=False) | ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=True)
-        approved_cos =  ChangeOrders.objects.filter(job_number=jobnumber,is_closed=False,is_approved=True)
+        open_cos = ChangeOrders.objects.filter(job_number=jobnumber, is_closed=False,
+                                               is_approved=False) & ChangeOrders.objects.filter(
+            is_t_and_m=False) | ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=True)
+        approved_cos = ChangeOrders.objects.filter(job_number=jobnumber, is_closed=False, is_approved=True)
         equipment = Inventory.objects.filter(job_number=jobnumber).order_by('inventory_type')
         rentals = Rentals.objects.filter(job_number=jobnumber)
-        wallcovering2 = Wallcovering.objects.filter(job_number = jobnumber)
+        wallcovering2 = Wallcovering.objects.filter(job_number=jobnumber)
         wc_not_ordereds = Wallcovering.objects.filter(job_number__job_number=jobnumber, orderitems1__isnull=True)
         # wc_not_ordereds = []
         # for x in wallcovering2:
@@ -88,10 +97,10 @@ def job_page(request,jobnumber):
         #     else:
         #         wc_not_ordereds.append(x)
         wc_ordereds = OrderItems.objects.filter(order__job_number=jobnumber, is_satisfied=False)
-        packages = Packages.objects.filter(delivery__order__job_number = jobnumber)
+        packages = Packages.objects.filter(delivery__order__job_number=jobnumber)
         deliveries = OutgoingItem.objects.filter(outgoing_event__job_number=jobnumber)
-        submittals = Submittals.objects.filter(job_number = jobnumber)
-        subcontracts = Subcontracts.objects.filter(job_number = jobnumber)
+        submittals = Submittals.objects.filter(job_number=jobnumber)
+        subcontracts = Subcontracts.objects.filter(job_number=jobnumber)
         return render(request, "job_page.html",
                       {'jobstable': jobstable,
                        'subcontracts': subcontracts, 'submittals': submittals, 'packages': packages,
@@ -101,13 +110,13 @@ def job_page(request,jobnumber):
 
 
 def book_new_job(request):
-        allclients = Clients.objects.order_by('company')[0:2000]
-        pms = ClientEmployees.objects.values('name', 'id', 'person_pk')[0:1000]
-        #send_employees = Employees.objects.filter(job_title="Superintendent")[0:2000]
-        prices_json = json.dumps(list(pms), cls=DjangoJSONEncoder)
+    allclients = Clients.objects.order_by('company')[0:2000]
+    pms = ClientEmployees.objects.values('name', 'id', 'person_pk')[0:1000]
+    # send_employees = Employees.objects.filter(job_title="Superintendent")[0:2000]
+    prices_json = json.dumps(list(pms), cls=DjangoJSONEncoder)
 
-        return render(request, 'book_new_job.html',
-                      {'data': prices_json, 'allclients': allclients, 'employees': {}})
+    return render(request, 'book_new_job.html',
+                  {'data': prices_json, 'allclients': allclients, 'employees': {}})
 
 
 def register(request):
@@ -123,11 +132,14 @@ def register(request):
                 new_number = int(JobNumbers.objects.latest("id").number) + 10
                 new_number_convert = str(new_number)
                 if new_number < 100:
-                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter, number="00" + new_number_convert)
+                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter,
+                                                number="00" + new_number_convert)
                 elif new_number < 1000:
-                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter, number="0" + new_number_convert)
+                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter,
+                                                number="0" + new_number_convert)
                 else:
-                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter, number=new_number_convert)
+                    new_job_number = JobNumbers(letter=JobNumbers.objects.latest("id").letter,
+                                                number=new_number_convert)
                 new_job_number.save()
                 job_number = JobNumbers.objects.latest("id").letter + JobNumbers.objects.latest("id").number
         else:
@@ -155,7 +167,7 @@ def register(request):
             checklist.append("waiting for contract")
         elif request.POST['contract_status'] == 'contract_received':
             contract_status = 1
-        else: #contract not required
+        else:  # contract not required
             contract_status = 3
 
         if request.POST['insurance_status'] == 'coi_not_received':
@@ -163,14 +175,15 @@ def register(request):
             checklist.append("get coi")
         elif request.POST['insurance_status'] == 'coi_received':
             insurance_status = 1
-        else: #contract not required
+        else:  # contract not required
             insurance_status = 3
 
         if request.POST['select_company'] == 'add_new':
             new_client = request.POST['new_client']
             new_client_phone = request.POST['new_client_phone']
             new_client_email = request.POST['new_client_bid_email']
-            client = ClientJobRoles.objects.create(company=new_client, bid_email=new_client_email,phone=new_client_phone)
+            client = ClientJobRoles.objects.create(company=new_client, bid_email=new_client_email,
+                                                   phone=new_client_phone)
             client.save();
         else:
             client = Clients.objects.get(id=request.POST['select_company'])
@@ -182,19 +195,19 @@ def register(request):
             new_name = request.POST['new_pm']
             new_phone = request.POST['new_pm_phone']
             new_email = request.POST['new_pm_email']
-            client_pm = ClientEmployees.objects.create(id=client, name=new_name, phone=new_phone,email=new_email)
+            client_pm = ClientEmployees.objects.create(id=client, name=new_name, phone=new_phone, email=new_email)
             client_pm.save;
         else:
             client_pm = ClientEmployees.objects.get(person_pk=request.POST['select_pm'])
 
         if request.POST['select_super'] == 'not_sure':
             checklist.append("get superintendent info")
-            client_super='not_sure'
+            client_super = 'not_sure'
         elif request.POST['select_super'] == 'use_below':
             new_name = request.POST['new_super']
             new_phone = request.POST['new_super_phone']
             new_email = request.POST['new_super_email']
-            client_super = ClientEmployees.objects.create(id=client, name=new_name, phone=new_phone,email=new_email)
+            client_super = ClientEmployees.objects.create(id=client, name=new_name, phone=new_phone, email=new_email)
             client_super.save;
 
         else:
@@ -202,51 +215,56 @@ def register(request):
 
         superintendent = request.POST['select_gpsuper']
         if request.POST['has_paint'] == 'true':
-                has_paint = True
+            has_paint = True
         else:
-                has_paint = False
+            has_paint = False
         if request.POST['has_wallcovering'] == 'true':
-                has_wallcovering = True
+            has_wallcovering = True
         else:
-                has_wallcovering = False
+            has_wallcovering = False
 
         start_date = request.POST['start_date']
 
-        #add email message request.POST['email_job_note']
+        # add email message request.POST['email_job_note']
         contract_amount = request.POST['contract_amount']
         painting_budget = request.POST['painting_budget']
         wallcovering_budget = request.POST['wallcovering_budget']
 
         job = Jobs.objects.create(job_number=job_number, job_name=job_name, address=address, city=city, state=state,
-                                   is_on_base=is_on_base, is_t_m_job=is_t_m_job, contract_status=contract_status,
-                                   insurance_status=insurance_status, client=client,
-                                   has_wallcovering=has_wallcovering, has_paint=has_paint, start_date=start_date,
-                                   status="Open", booked_date=date.today())
+                                  is_on_base=is_on_base, is_t_m_job=is_t_m_job, contract_status=contract_status,
+                                  insurance_status=insurance_status, client=client,
+                                  has_wallcovering=has_wallcovering, has_paint=has_paint, start_date=start_date,
+                                  status="Open", booked_date=date.today())
         if client_super != 'not_sure':
-            job.client_super= client_super
+            job.client_super = client_super
         if client_pm != 'not_sure':
-            job.client_pm=client_pm
-            job.client_submittal_contact=client_pm
-            job.client_co_contact=client_pm
+            job.client_pm = client_pm
+            job.client_submittal_contact = client_pm
+            job.client_co_contact = client_pm
         if is_t_m_job == False:
-            job.contract_amount=contract_amount
+            job.contract_amount = contract_amount
         if t_m_nte_amount != "":
-            job.t_m_nte_amount=t_m_nte_amount
+            job.t_m_nte_amount = t_m_nte_amount
         if spray_scale != "":
-            job.spray_scale=spray_scale
+            job.spray_scale = spray_scale
         if brush_role != "":
-            job.brush_role=brush_role
+            job.brush_role = brush_role
         if painting_budget != "":
-            job.painting_budget=painting_budget
+            job.painting_budget = painting_budget
         if wallcovering_budget != "":
-            job.wallcovering_budget=wallcovering_budget
+            job.wallcovering_budget = wallcovering_budget
         if superintendent != 'not_sure':
-            job.superintendent=Employees.objects.get(id=superintendent)
-        JobNotes.objects.create(job_number=job, note = "Start Date at Booking: " + start_date + " " + request.POST['date_note'], type="auto_start_date_note", date = date.today(),user=request.user.first_name + " " + request.user.last_name)
+            job.superintendent = Employees.objects.get(id=superintendent)
+        JobNotes.objects.create(job_number=job,
+                                note="Start Date at Booking: " + start_date + " " + request.POST['date_note'],
+                                type="auto_start_date_note", date=date.today(),
+                                user=request.user.first_name + " " + request.user.last_name)
 
         JobNotes.objects.create(job_number=job,
-                                 note="New Job Booked By: " + request.user.first_name + " " + request.user.last_name + ": " + request.POST['email_job_note'],
-                                 type="auto_booking_note", date=date.today(), user=request.user.first_name + " " + request.user.last_name)
+                                note="New Job Booked By: " + request.user.first_name + " " + request.user.last_name + ": " +
+                                     request.POST['email_job_note'],
+                                type="auto_booking_note", date=date.today(),
+                                user=request.user.first_name + " " + request.user.last_name)
         email_body = "New Job Booked \n" + job.job_number + "\n" + job.job_name + "\n" + job.client.company
         Email.sendEmail("New Job - " + job.job_name, email_body, 'joe@gerloffpainting.com')
         # for x in checklist:
@@ -254,6 +272,7 @@ def register(request):
         #     checklist.save();
         response = redirect('/')
         return response
+
 
 class Checklist(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -279,4 +298,3 @@ class Checklist(models.Model):
 
     def __str__(self):
         return f"{self.job_number} {self.checklist_item}"
-
