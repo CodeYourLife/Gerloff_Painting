bd506781d838fff46ffc7faeefa25103462c0199
joe <joe@gerloffpainting.com>
2023-09-24 19:38:02 -0400

    user registration done

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("registration", views.registration, name='registration'),
     path("verify_pin", views.verifyPin, name='verify_pin'),
-    path("add_employee", views.addEmployee, name='add_employee'),
-    path("addNewEmployee", views.addNewEmployee, name='addNewEmployee'),
+    # path("add_employee", views.addEmployee, name='add_employee'),
+    # path("addNewEmployee", views.addNewEmployee, name='addNewEmployee'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
