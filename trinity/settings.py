fcc4f471434aa848f81eebbe8bdfd29a7bb6a147
anthonytaroli <taroli757@gmail.com>
2023-10-06 15:17:58 -0400

    Introduce removal of jssignature

--- trinity/settings.py
+++ trinity/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'console.apps.ConsoleConfig',
-    'jsignature',
     'jobs',
     'changeorder',
     'equipment',
