aabd6405ea526f08815f48de54e781d378ec3460
Anthony Taroli <taroli757@gmail.com>
2023-10-16 19:50:51 -0400

    fixed job booking (#47)
    Co-authored-by: Joe <joe@gerloffpainting.com>

--- trinity/settings.py
+++ trinity/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3t5e#t@#xt)=#557w!n_y@vrm-c8x7_71z=8tw-0+(_^&6&$_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.168.120', '98.174.216.233']
 
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Nailset1!',
+        'PASSWORD': 'Password4880',
         'HOST': 'localhost'
     }
 }
