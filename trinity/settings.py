956167e00bf4980cad89f441987bf2130f66e14b
Anthony Taroli <taroli757@gmail.com>
2023-10-19 17:55:39 -0400

    added logo (#61)
    Co-authored-by: Joe <joe@gerloffpainting.com>

--- trinity/settings.py
+++ trinity/settings.py
@@ -132,14 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-STATICFILES_DIR = (
-    '/jobs/static/jobs/project_styles.css'
-)
+STATICFILES_DIR = [
+BASE_DIR / "static"
+]
+#STATICFILES_DIR =['/jobs/static/jobs/project_styles.css']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # For Emails
