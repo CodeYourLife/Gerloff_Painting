78b6ea23823a258835764e7f8b68bcf32b6fccfc
joe <joe@gerloffpainting.com>
2023-10-19 09:12:48 -0400

    changed superintendent page again

--- superintendent/views.py
+++ superintendent/views.py
@@ -19,13 +19,22 @@ def super_home(request, super, filter):
         else:
             selected_superid = request.POST['selected_super']
     if selected_superid == 'ALL':
+        send_data['equipment'] = Inventory.objects.exclude(job_number=None)
+        send_data['rentals'] = Rentals.objects.filter(off_rent_date__isnull=True)
+        send_data['tickets'] = ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=False, is_closed=False)
         if filter == 'UPCOMING':
             send_data['filtered'] = 'filtered'
             jobs = Jobs.objects.filter(is_active=False).order_by('start_date')
         else:
             jobs = Jobs.objects.all().order_by('start_date')
     else:
-        send_data['selected_super'] = Employees.objects.get(id=selected_superid)
+        selected_super = Employees.objects.get(id=selected_superid)
+        send_data['equipment'] = Inventory.objects.filter(job_number__superintendent=selected_super)
+        send_data['selected_super'] = selected_super
+        send_data['rentals'] = Rentals.objects.filter(job_number__superintendent=selected_super,
+                                                      off_rent_date__isnull=True)
+        send_data['tickets'] = ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=False, is_closed=False,
+                                                           job_number__superintendent=selected_super)
         if filter == 'UPCOMING':
             send_data['filtered'] = 'filtered'
             jobs = Jobs.objects.filter(is_active=False,
@@ -36,9 +45,6 @@ def super_home(request, super, filter):
                 'start_date')
     send_data['jobs'] = jobs
     send_data['supers'] = Employees.objects.exclude(job_title__description="Painter")
-    send_data['equipment'] = Inventory.objects.exclude(job_number=None)
-    send_data['rentals'] = Rentals.objects.filter(off_rent_date__isnull=True)
-    send_data['tickets'] = ChangeOrders.objects.filter(is_t_and_m=True, is_ticket_signed=False, is_closed=False)
     send_data['todays_date'] = date.today() - timedelta(days=45)
     return render(request, "super_home.html", send_data)
 
@@ -48,7 +54,6 @@ def filter_super(request):
     if request.method == 'POST':
         jobs = Jobs.objects.filter(superintendent=request.POST['selected_super'])[0:2000]
         supers = Employees.objects.all()[0:2000]
-        # equipmentlist = Inventory.objects.exclude(job_number = None)
         selected_super = Employees.objects.get(id=request.POST['selected_super'])
         equipment = Inventory.objects.filter(job_number__superintendent=selected_super)
         rentals = Rentals.objects.filter(job_number__superintendent=selected_super, off_rent_date__isnull=True)
