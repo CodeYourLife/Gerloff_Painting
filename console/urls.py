3fc607ab9928db98d8e938d62a975e077f23b61b
joe <joe@gerloffpainting.com>
2023-10-27 17:19:49 -0400

    create folders for equipment imported

--- console/urls.py
+++ console/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('admin_home', views.admin_home, name='admin_home'),
     path('grant_web_access', views.grant_web_access, name='grant_web_access'),
     path('reset_databases', views.reset_databases, name='reset_databases'),
-
+    path('create_folders', views.create_folders, name='create_folders'),
 
 ]
