43e6b2bf6f6c7f579aec815615d14f91716a9f99
Anthony Taroli <taroli757@gmail.com>
2023-10-30 15:23:20 -0400

    Introduce awesome jobs table and filters (#104)
    * Introduce jobs table styling for filters
    
    * Introduce jobs table styling for cells

--- equipment/filters.py
+++ equipment/filters.py
@@ -1,9 +1,3 @@
-from jobs.models import Jobs
-from equipment.models import InventoryNotes, Inventory
-import django_filters
-from django.db.models import Q
-
-
 class JobsFilter2(django_filters.FilterSet):
     search = django_filters.CharFilter(label='Job Name =', method='search_filter')
     search4 = django_filters.CharFilter(label='GC =', method='search_filter4')
@@ -21,11 +15,16 @@ class JobsFilter2(django_filters.FilterSet):
 
 
 class JobsFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(label='Job Name', method='search_filter')
+    search = django_filters.CharFilter(label='Job Name', method='search_filter',
+                                       widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     search2 = django_filters.ChoiceFilter(label="Job Status", method='search_filter2',
-                                          choices=((0, "Open Jobs Only"), (1, "All Jobs")),empty_label="Open Jobs")
-    search3 = django_filters.CharFilter(label='Super', method='search_filter3')
-    search4 = django_filters.CharFilter(label='GC', method='search_filter4')
+                                          choices=((0, "Open Jobs Only"), (1, "All Jobs")),
+                                          empty_label="Open Jobs")
+    search3 = django_filters.CharFilter(label='Super', method='search_filter3',
+                                        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    search4 = django_filters.CharFilter(label='GC', method='search_filter4',
+                                        widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     def search_filter(self, queryset, name, value):
         return queryset.filter(job_name__icontains=value)
@@ -39,15 +38,15 @@ class JobsFilter(django_filters.FilterSet):
             return queryset.filter(is_closed=False)
 
     def search_filter3(self, queryset, name, value):
-        return queryset.filter(Q(superintendent__first_name__icontains=value) | Q(superintendent__last_name__icontains=value))
+        return queryset.filter(
+            Q(superintendent__first_name__icontains=value) | Q(superintendent__last_name__icontains=value))
 
     def search_filter4(self, queryset, name, value):
         return queryset.filter(client__company__icontains=value)
 
-
     class Meta:
         model = Jobs
-        fields = ['search','search2','search3','search4']
+        fields = ['search', 'search2', 'search3', 'search4']
 
 
 class EquipmentNotesFilter(django_filters.FilterSet):
@@ -88,3 +87,4 @@ class EquipmentFilter2(django_filters.FilterSet):
     class Meta:
         model = Inventory
         fields = ['search', 'search2', 'search3']
+
