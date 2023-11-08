3a08612c0c578aadfea4f2b950c5c13167ba05c5
joe <joe@gerloffpainting.com>
2023-11-08 08:17:32 -0500

    fixed filters and modals

--- superintendent/urls.py
+++ superintendent/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 
-    path('super_home/<super> <filter>', views.super_home, name='super_home'),
+    path('super_home/<super>', views.super_home, name='super_home'),
     path('filter_super', views.filter_super, name='filter_super'),
     path('super_ajax', views.super_ajax, name='super_ajax'),
 ]
