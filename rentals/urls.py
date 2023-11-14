d0ce49449211b005e2182738625ef12123dda73d
joe <joe@gerloffpainting.com>
2023-11-14 10:40:28 -0500

    see rental vendor info

--- rentals/urls.py
+++ rentals/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
 path("rentals_home", views.rentals_home, name='rentals_home'),
+path("rental_ajax", views.rental_ajax, name='rental_ajax'),
 path("rental_new/<jobnumber>", views.rental_new, name='rental_new'),
 path("rental_page/<id> <reverse>", views.rental_page, name='rental_page'),#reverse = 'YES' or 'NO'
 ]
--- rentals/views.py
+++ rentals/views.py
@@ -1,4 +1,3 @@
-
 from equipment.models import Vendors, VendorContact, VendorCategory
 from rentals.models import Rentals, RentalNotes
 from jobs.models import Jobs
@@ -16,16 +15,18 @@ from django.conf import settings
 from employees.models import *
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 @login_required(login_url='/accounts/login')
 def rentals_home(request):
-    table = RentalsTable(Rentals.objects.filter(is_closed=False).order_by('job_number','company'))
+    table = RentalsTable(Rentals.objects.filter(is_closed=False).order_by('job_number', 'company'))
     RequestConfig(request).configure(table)
     return render(request, "rentals_home.html", {'table': table})
 
+
 @login_required(login_url='/accounts/login')
-def rental_new(request,jobnumber):
+def rental_new(request, jobnumber):
     if jobnumber == "ALL":
         jobs = Jobs.objects.filter(is_closed=False).order_by('job_name')
     else:
@@ -34,68 +35,113 @@ def rental_new(request,jobnumber):
     pms_json = json.dumps(list(VendorContact.objects.values('name', 'id', 'company')), cls=DjangoJSONEncoder)
     if request.method == 'POST':
         if 'search_job' in request.POST:
-            jobs = Jobs.objects.filter(is_closed=False,job_name__icontains=request.POST['search_job']).order_by('job_name')
+            jobs = Jobs.objects.filter(is_closed=False, job_name__icontains=request.POST['search_job']).order_by(
+                'job_name')
             return render(request, "rental_new.html", {'jobs': jobs, 'vendors': vendors, 'data': pms_json})
         else:
             if request.POST['select_company'] == 'add_new':
-                vendor = Vendors.objects.create(company_name = request.POST['new_client'],category=VendorCategory.objects.get(category = "Equipment Rental"),company_phone=request.POST['new_client_phone'],company_email=request.POST['new_client_bid_email'])
+                vendor = Vendors.objects.create(company_name=request.POST['new_client'],
+                                                category=VendorCategory.objects.get(category="Equipment Rental"),
+                                                company_phone=request.POST['new_client_phone'],
+                                                company_email=request.POST['new_client_bid_email'])
             else:
                 vendor = Vendors.objects.get(id=request.POST['select_company'])
-            rental = Rentals.objects.create(company=vendor, job_number=Jobs.objects.get(job_number=request.POST['select_job']),item=request.POST['item'],on_rent_date=request.POST['on_rent_date'], notes = request.POST['notes'])
+                vendor.company_name=request.POST['new_client']
+                vendor.company_phone=request.POST['new_client_phone']
+                vendor.company_email=request.POST['new_client_bid_email']
+                vendor.save()
+            rental = Rentals.objects.create(company=vendor,
+                                            job_number=Jobs.objects.get(job_number=request.POST['select_job']),
+                                            item=request.POST['item'], on_rent_date=request.POST['on_rent_date'],
+                                            notes=request.POST['notes'])
             if request.POST['select_pm'] != 'no_rep':
                 if request.POST['select_pm'] == 'add_new':
-                    rental.rep = VendorContact.objects.create(company=vendor,name=request.POST['new_pm'],email=request.POST['new_pm_email'],phone=request.POST['new_pm_phone'])
+                    rental.rep = VendorContact.objects.create(company=vendor, name=request.POST['new_pm'],
+                                                              email=request.POST['new_pm_email'],
+                                                              phone=request.POST['new_pm_phone'])
                 else:
-                    rental.rep = VendorContact.objects.get(id = request.POST['select_pm'])
+                    rental.rep = VendorContact.objects.get(id=request.POST['select_pm'])
+                    rental.rep.name=request.POST['new_pm']
+                    rental.rep.phone=request.POST['new_pm_phone']
+                    rental.rep.email=request.POST['new_pm_email']
+                    rental.rep.save()
 
-            if request.POST['purchase_order'] != '':rental.purchase_order=request.POST['purchase_order']
-            if request.POST['notes']!= '': rental.notes=request.POST['notes']
-            if request.POST['day_price']!= '': rental.day_price=request.POST['day_price']
-            if request.POST['week_price']!= '':rental.week_price=request.POST['week_price']
-            if request.POST['month_price']!= '':rental.month_price=request.POST['month_price']
+            if request.POST['purchase_order'] != '': rental.purchase_order = request.POST['purchase_order']
+            if request.POST['notes'] != '': rental.notes = request.POST['notes']
+            if request.POST['day_price'] != '': rental.day_price = request.POST['day_price']
+            if request.POST['week_price'] != '': rental.week_price = request.POST['week_price']
+            if request.POST['month_price'] != '': rental.month_price = request.POST['month_price']
             rental.save()
-            RentalNotes.objects.create(rental=rental,date=date.today(),user=Employees.objects.get(user=request.user),note="New Rental Added. " + request.POST['notes'])
+            RentalNotes.objects.create(rental=rental, date=date.today(), user=Employees.objects.get(user=request.user),
+                                       note="New Rental Added. " + request.POST['notes'])
             createfolder("rentals/" + str(rental.id))
-            return redirect("rental_page",id=rental.id,reverse='NO')
+            return redirect("rental_page", id=rental.id, reverse='NO')
     else:
-        return render(request, "rental_new.html", {'jobs':jobs,'vendors':vendors,'data':pms_json})
+        return render(request, "rental_new.html", {'jobs': jobs, 'vendors': vendors, 'data': pms_json})
+
+
+def rental_ajax(request):
+    if request.is_ajax():
+        send_data = {}
+        if 'rep_id' in request.GET:
+            send_data['phone'] = VendorContact.objects.get(id=request.GET['rep_id']).phone
+            send_data['email'] = VendorContact.objects.get(id=request.GET['rep_id']).email
+            send_data['name'] = VendorContact.objects.get(id=request.GET['rep_id']).name
+        if 'vendor_id' in request.GET:
+            send_data['phone'] = Vendors.objects.get(id=request.GET['vendor_id']).company_phone
+            send_data['email'] = Vendors.objects.get(id=request.GET['vendor_id']).company_email
+            send_data['name'] = Vendors.objects.get(id=request.GET['vendor_id']).company_name
+        return HttpResponse(json.dumps(send_data))
 
 
 @login_required(login_url='/accounts/login')
-def rental_page(request,id,reverse):
+def rental_page(request, id, reverse):
     rental = Rentals.objects.get(id=id)
     reverse = reverse
     reps = VendorContact.objects.filter(company=rental.company)
     vendor = rental.company
     notes = RentalNotes.objects.filter(rental=rental)
     path = os.path.join(settings.MEDIA_ROOT, "rentals", str(rental.id))
-    foldercontents =  os.listdir(path)
+    foldercontents = os.listdir(path)
+
     if request.method == 'POST':
         if 'form_1' in request.POST:
-            note=""
+            note = ""
+            rental.company.company_phone = request.POST['company_phone']
+            rental.company.company_email = request.POST['company_email']
+            rental.company.save()
             if request.POST['select_pm'] != 'no_rep':
                 if request.POST['select_pm'] == 'add_new':
-                    rental.rep = VendorContact.objects.create(company=vendor,name=request.POST['new_pm'],email=request.POST['new_pm_email'],phone=request.POST['new_pm_phone'])
+                    rental.rep = VendorContact.objects.create(company=vendor, name=request.POST['new_pm'],
+                                                              email=request.POST['new_pm_email'],
+                                                              phone=request.POST['new_pm_phone'])
                 else:
-                    rental.rep = VendorContact.objects.get(id = request.POST['select_pm'])
+                    rental.rep = VendorContact.objects.get(id=request.POST['select_pm'])
+                    rental.rep.name = request.POST['new_pm']
+                    rental.rep.email = request.POST['new_pm_email']
+                    rental.rep.phone = request.POST['new_pm_phone']
+                    rental.rep.save()
+            else:
+                if rental.rep:
+                    rental.rep = None
             if request.POST['purchase_order'] != '':
                 note = "Purchase Order Entered. "
-                rental.purchase_order=request.POST['purchase_order']
+                rental.purchase_order = request.POST['purchase_order']
             if request.POST['off_rent_date'] != '':
                 note = note + " Off Rent Date Entered. "
-                rental.off_rent_date=request.POST['off_rent_date']
-            if request.POST['off_rent_number']!= '':
+                rental.off_rent_date = request.POST['off_rent_date']
+            if request.POST['off_rent_number'] != '':
                 note = note + " Off Rent Number Entered. "
-                rental.off_rent_number=request.POST['off_rent_number']
-            if request.POST['day_price']!= '':
+                rental.off_rent_number = request.POST['off_rent_number']
+            if request.POST['day_price'] != '':
                 note = note + " Day Price Entered. "
-                rental.day_price=request.POST['day_price']
-            if request.POST['week_price']!= '':
+                rental.day_price = request.POST['day_price']
+            if request.POST['week_price'] != '':
                 note = note + " Week Price Entered. "
-                rental.week_price=request.POST['week_price']
-            if request.POST['month_price']!= '':
+                rental.week_price = request.POST['week_price']
+            if request.POST['month_price'] != '':
                 note = note + " Month Price Entered. "
-                rental.month_price=request.POST['month_price']
+                rental.month_price = request.POST['month_price']
             if 'is_closed' in request.POST:
                 note = note + " Billed. "
                 rental.is_closed = True
@@ -115,4 +161,5 @@ def rental_page(request,id,reverse):
             fn2 = os.path.join(settings.MEDIA_ROOT, "rentals", str(rental.id), fn)
             open(fn2, 'wb').write(fileitem.file.read())
         return redirect("rental_page", id=rental.id, reverse='YES')
-    return render(request, "rental_page.html", {'rental': rental, 'reverse':reverse,'reps':reps,'notes':notes,'foldercontents':foldercontents})
+    return render(request, "rental_page.html", {'rental': rental, 'reverse': reverse, 'reps': reps, 'notes': notes,
+                                                'foldercontents': foldercontents})
