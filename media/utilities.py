1b173f30a0e4a61ce703d1971034773fa20f8b03
Anthony Taroli <taroli757@gmail.com>
2026-01-02 14:22:31 -0500

    T173 (#409)
    Co-authored-by: joe <joe@gerloffpainting.com>

--- media/utilities.py
+++ media/utilities.py
@@ -15,6 +15,11 @@ class MediaUtilities(object):
         if os.path.exists(file_path):
             name = value.split('.')[0]
             mimetype = value.split('.')[1]
-            with open(file_path, 'rb') as fh:
-                return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'inline; filename="{name}.{mimetype}"'})
-                #return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'attachment; filename="{name}.{mimetype}"'})
+            if mimetype == "pdf":
+                with open(file_path, 'rb') as fh:
+                    return HttpResponse(fh.read(), headers={'Content-Type': f'application/{mimetype}','Content-Disposition': f'inline'})
+                    #return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'attachment; filename="{name}.{mimetype}"'})
+            else:
+                with open(file_path, 'rb') as fh:
+                    return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'inline; filename="{name}.{mimetype}"'})
+                    #return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'attachment; filename="{name}.{mimetype}"'})
