2e1b2cd4cce9150b8db693373d266fc6c6be26ea
joe <joe@gerloffpainting.com>
2026-02-04 08:35:02 -0500

    fixed admin panel (removed extra stuff). added skip to emails for safety inspections

--- changeorder/admin.py
+++ changeorder/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(TMPricesMaster)
-admin.site.register(TMList)
-admin.site.register(TMProposal)
-admin.site.register(EWTicket)
-admin.site.register(EWT)
-admin.site.register(ClientJobRoles)
-admin.site.register(ChangeOrders)
-admin.site.register(TempRecipients)
-admin.site.register(ChangeOrderNotes)
+# admin.site.register(TMPricesMaster)
+# admin.site.register(TMList)
+# admin.site.register(TMProposal)
+# admin.site.register(EWTicket)
+# admin.site.register(EWT)
+# admin.site.register(ClientJobRoles)
+# admin.site.register(ChangeOrders)
+# admin.site.register(TempRecipients)
+# admin.site.register(ChangeOrderNotes)
 
 
 
