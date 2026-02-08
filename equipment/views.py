66b870dd523730d5c654b3a4a9fe66e7f0a818d2
joe <joe@gerloffpainting.com>
2026-02-08 12:52:31 -0500

    created plans shortcuts in change order folder

--- equipment/views.py
+++ equipment/views.py
@@ -597,7 +597,7 @@ def equipment_page(request, id):
     employees = Employees.objects.filter(active=True)
     vendors = Vendors.objects.filter(category__category="Equipment Repair")
     path = os.path.join(settings.MEDIA_ROOT, "equipment", str(inventory.id))
-    send_data['folder_path'] = path
+    send_data['folder_path'] = rf"\\gp-webserver\trinity\equipment\{inventory.id}"
     foldercontents = os.listdir(path)
     folder_count=0
     for x in os.listdir(path):
