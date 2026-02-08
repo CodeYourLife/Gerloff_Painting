2c81e20bcf025801dca107e46e5acb5893fcd44e
joe <joe@gerloffpainting.com>
2026-02-08 17:20:39 -0500

    django fix

--- trinity/wsgi.py
+++ trinity/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for trinity project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# ADD THIS LINE — project root
+sys.path.append(r"D:\Gerloff_Painting")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trinity.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trinity.settings')
 
 application = get_wsgi_application()
