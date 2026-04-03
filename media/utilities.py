e5df51a05ed83ea0f73c21f4a9f92f4cf6a981a4
Anthony Taroli <taroli757@gmail.com>
2026-04-03 12:23:41 -0400

    T287 (#502)
    * added submittal functionality
    
    # Conflicts:
    #	submittals/templates/submittals_home.html
    
    * added submittal functionality
    
    * fixed change order price sending 0, added button for paper ticket signed, and fixed directory to open files with alot of periods
    
    ---------
    
    Co-authored-by: joe <joe@gerloffpainting.com>

--- media/utilities.py
+++ media/utilities.py
@@ -2,31 +2,81 @@ from django.core.files.storage import FileSystemStorage
 import os
 import os.path
 import csv
+import mimetypes
 from pathlib import Path
 from django.conf import settings
 from django.http import HttpResponse
 
 from changeorder.models import ChangeOrders
 
+#------OLD DELETED 4.3.26
+# class MediaUtilities(object):
+#
+#     def __init__(self):
+#         super(MediaUtilities, self).__init__()
+#     def getDirectoryContents(self, id, value,app):
+#         if app=="changeorder":
+#             changeorder=ChangeOrders.objects.get(id=id)
+#             file_path = os.path.join(settings.MEDIA_ROOT, app, rf"{changeorder.job_number.job_number} COP #{changeorder.cop_number}", os.path.basename(value))
+#         else:
+#             file_path = os.path.join(settings.MEDIA_ROOT, app, str(id), os.path.basename(value))
+#         if os.path.exists(file_path):
+#             name = value.split('.')[0]
+#             mimetype = value.split('.')[1]
+#             if mimetype == "pdf":
+#                 with open(file_path, 'rb') as fh:
+#                     return HttpResponse(fh.read(), headers={'Content-Type': f'application/{mimetype}','Content-Disposition': f'inline'})
+#
+#             else:
+#                 with open(file_path, 'rb') as fh:
+#                     return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'inline; filename="{name}.{mimetype}"'})
+#
+
+import os
+import mimetypes
+from django.http import HttpResponse
+from django.conf import settings
 
 class MediaUtilities(object):
 
     def __init__(self):
         super(MediaUtilities, self).__init__()
-    def getDirectoryContents(self, id, value,app):
-        if app=="changeorder":
-            changeorder=ChangeOrders.objects.get(id=id)
-            file_path = os.path.join(settings.MEDIA_ROOT, app, rf"{changeorder.job_number.job_number} COP #{changeorder.cop_number}", os.path.basename(value))
+
+    def getDirectoryContents(self, id, value, app):
+        if app == "changeorder":
+            changeorder = ChangeOrders.objects.get(id=id)
+            file_path = os.path.join(
+                settings.MEDIA_ROOT,
+                app,
+                rf"{changeorder.job_number.job_number} COP #{changeorder.cop_number}",
+                os.path.basename(value)
+            )
         else:
-            file_path = os.path.join(settings.MEDIA_ROOT, app, str(id), os.path.basename(value))
+            file_path = os.path.join(
+                settings.MEDIA_ROOT,
+                app,
+                str(id),
+                os.path.basename(value)
+            )
+
         if os.path.exists(file_path):
-            name = value.split('.')[0]
-            mimetype = value.split('.')[1]
-            if mimetype == "pdf":
-                with open(file_path, 'rb') as fh:
-                    return HttpResponse(fh.read(), headers={'Content-Type': f'application/{mimetype}','Content-Disposition': f'inline'})
-                    #return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'attachment; filename="{name}.{mimetype}"'})
+            filename = os.path.basename(value)
+            content_type, _ = mimetypes.guess_type(file_path)
+
+            if not content_type:
+                content_type = "application/octet-stream"
+
+            disposition = "inline"
+            if not content_type.startswith("image/") and content_type != "application/pdf":
+                disposition = f'attachment; filename="{filename}"'
             else:
-                with open(file_path, 'rb') as fh:
-                    return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'inline; filename="{name}.{mimetype}"'})
-                    #return HttpResponse(fh.read(), headers={'Content-Type': f'image/{mimetype}','Content-Disposition': f'attachment; filename="{name}.{mimetype}"'})
+                disposition = f'inline; filename="{filename}"'
+
+            with open(file_path, 'rb') as fh:
+                return HttpResponse(
+                    fh.read(),
+                    headers={
+                        "Content-Type": content_type,
+                        "Content-Disposition": disposition,
+                    }
+                )
